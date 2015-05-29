1d88cdff0b0d1a89eea9fecadf64be52dface90f
Jukaligrupo1 <julieticaromero.31@gmail>
2015-05-29 12:08:41 -0500

    correcion jkali

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,8 +16,20 @@ Route::get('/', 'WelcomeController@index');
 Route::get('home', 'HomeController@index');
 
 Route::controller('entidades', 'EntidadesController');
-Route::controller('disciplinas', 'DisciplinasController');
+Route::controller('disciplinas', 'disciplinasController');
+Route::controller('disciplinas_traduc', 'disciplinas_traducController');
+Route::controller('niveles', 'nivelesController');
+Route::controller('niveles_traduc', 'niveles_traducController');
+Route::controller('categorias', 'categoriasController');
+Route::controller('categorias_traduc', 'categorias_traducController');
 Route::controller('eventos', 'EventosController');
+Route::controller('nivel_participante', 'nivel_participanteController');
+Route::controller('inscripciones', 'inscripcionesController');
+Route::controller('user_event', 'user_eventController');
+Route::controller('evaluaciones', 'EventosController');
+Route::controller('pregunta_evaluacion', 'pregunta_evaluacionController');
+Route::controller('examenes', 'ExamenesController');
+
 
 
 
